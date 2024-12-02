6186a50945ab2efd19242606a7d80fb717d35fdb
Sovereign Shahid <sovereignshahid@gmail.com>
2024-12-02 12:40:52 -0700

    refactored day 2

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,5 +1,4 @@
 enum Change {
-    None,
     Inc,
     Dec,
 }
@@ -7,41 +6,38 @@ fn code(input: &str, allow_problem: i32) -> i32 {
     let lines = input.split('\n');
     let mut safe = 0;
     for line in lines {
-        let levels: Vec<i32> = line.split(" ").map(|x| x.parse().unwrap()).collect();
-
-        let mut is_safe = true;
-        let mut change = Change::None;
-
-        for level in levels.windows(2) {
-            let diff = level[1] - level[0];
-            if diff.abs() > 3 || diff.abs() < 1 {
-                is_safe = false;
-            }
-            match change {
-                Change::None => change = if (diff) > 0 { Change::Inc } else { Change::Dec },
-                Change::Inc => {
-                    if (diff) > 0 {
-                    } else {
-                        is_safe = false;
-                    }
-                }
-                Change::Dec => {
-                    if (diff) > 0 {
-                        is_safe = false;
-                    } else {
-                    }
-                }
-            }
-        }
-        if is_safe {
+        if is_line_safe(line) {
             safe += 1;
         }
     }
     return safe;
 }
 
-fn pt2(input: &str) -> i32 {
-    0
+fn is_line_safe(line: &str) -> bool {
+    let levels: Vec<i32> = line.split(" ").map(|x| x.parse().unwrap()).collect();
+    let change = if (levels[1] - levels[0]).signum() == 1 {
+        Change::Inc
+    } else {
+        Change::Dec
+    };
+    for level in levels.windows(2) {
+        let diff = level[1] - level[0];
+        if diff.abs() > 3 || diff.abs() < 1 {
+            return false;
+        }
+        match diff.signum() {
+            1 => match change {
+                Change::Inc => continue,
+                Change::Dec => return false,
+            },
+            -1 => match change {
+                Change::Inc => return false,
+                Change::Dec => continue,
+            },
+            _ => panic!("should not reach here"),
+        };
+    }
+    return true;
 }
 
 #[cfg(test)]
@@ -69,11 +65,11 @@ mod test {
     #[test]
     fn pt2_example() {
         let input = r#"7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9"#;
+    1 2 7 8 9
+    9 7 6 2 1
+    1 3 2 4 5
+    8 6 4 4 1
+    1 3 6 7 9"#;
         let output = code(input, 1);
         println!("{}", output);
         assert!(output == 4);
