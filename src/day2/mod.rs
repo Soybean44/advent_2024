96da03581d2bb584073b5ada83304f784e8453f5
Sovereign Shahid <sovereignshahid@gmail.com>
2024-12-03 09:08:48 -0700

    attempted pt 2 day 2

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -2,40 +2,78 @@ enum Change {
     Inc,
     Dec,
 }
-fn code(input: &str, allow_problem: i32) -> i32 {
+fn pt1(input: &str) -> i32 {
     let lines = input.split('\n');
     let mut safe = 0;
     for line in lines {
-        if is_line_safe(line) {
+        let levels: Vec<i32> = line.split(" ").map(|x| x.parse().unwrap()).collect();
+        if is_line_safe(levels) {
             safe += 1;
         }
     }
     return safe;
 }
 
-fn is_line_safe(line: &str) -> bool {
-    let levels: Vec<i32> = line.split(" ").map(|x| x.parse().unwrap()).collect();
+fn pt2(input: &str) -> i32 {
+    let lines = input.split('\n');
+    let mut safe = 0;
+    for line in lines {
+        let levels: Vec<i32> = line.split(" ").map(|x| x.parse().unwrap()).collect();
+        if is_line_less_safe(levels) {
+            safe += 1;
+        }
+    }
+    return safe;
+}
+
+fn pair_safe(l1: i32, l2: i32, change: &Change) -> bool {
+    let diff = l2 - l1;
+    if diff.abs() > 3 || diff.abs() < 1 {
+        return false;
+    }
+    match diff.signum() {
+        1 => match change {
+            Change::Inc => return true,
+            Change::Dec => return false,
+        },
+        -1 => match change {
+            Change::Inc => return false,
+            Change::Dec => return true,
+        },
+        _ => panic!("should not reach here"),
+    };
+}
+
+fn is_line_safe(levels: Vec<i32>) -> bool {
     let change = if (levels[1] - levels[0]).signum() == 1 {
         Change::Inc
     } else {
         Change::Dec
     };
-    for level in levels.windows(2) {
-        let diff = level[1] - level[0];
-        if diff.abs() > 3 || diff.abs() < 1 {
+    for i in 0..levels.len() - 1 {
+        if !pair_safe(levels[i], levels[i + 1], &change) {
             return false;
         }
-        match diff.signum() {
-            1 => match change {
-                Change::Inc => continue,
-                Change::Dec => return false,
-            },
-            -1 => match change {
-                Change::Inc => return false,
-                Change::Dec => continue,
-            },
-            _ => panic!("should not reach here"),
-        };
+    }
+    return true;
+}
+
+fn is_line_less_safe(levels: Vec<i32>) -> bool {
+    let change = if (levels[1] - levels[0]).signum() == 1 {
+        Change::Inc
+    } else {
+        Change::Dec
+    };
+    for i in 0..levels.len() - 1 {
+        if !pair_safe(levels[i], levels[i + 1], &change) {
+            let mut l1 = levels.clone();
+            l1.remove(i);
+            let mut l2 = levels.clone();
+            l2.remove(i + 1);
+            if !is_line_safe(l1) && !is_line_safe(l2) {
+                return false;
+            }
+        }
     }
     return true;
 }
@@ -51,34 +89,34 @@ mod test {
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9"#;
-        let output = code(input, 0);
+        let output = pt1(input);
         println!("{}", output);
         assert!(output == 2);
     }
     #[test]
     fn pt1_test() {
         let input = include_str!("pt1.txt").trim_end();
-        let output = code(input, 0);
+        let output = pt1(input);
         println!("{}", output);
         assert!(output == 490);
     }
     #[test]
     fn pt2_example() {
         let input = r#"7 6 4 2 1
-    1 2 7 8 9
-    9 7 6 2 1
-    1 3 2 4 5
-    8 6 4 4 1
-    1 3 6 7 9"#;
-        let output = code(input, 1);
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"#;
+        let output = pt2(input);
         println!("{}", output);
         assert!(output == 4);
     }
-    //#[test]
-    //fn pt2_test() {
-    //    let input = include_str!("pt1.txt").trim_end();
-    //    let output = code(input, 1);
-    //    println!("{}", output);
-    //    assert!(output == 26593248);
-    //}
+    #[test]
+    fn pt2_test() {
+        let input = include_str!("pt1.txt").trim_end();
+        let output = pt2(input);
+        println!("{}", output);
+        assert!(output == 529); // this is wrong
+    }
 }
