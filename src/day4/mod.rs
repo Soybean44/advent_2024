96592c6a6caa4c4f873921515f3884887de3c578
Sovereign Shahid <sovereignshahid@gmail.com>
2024-12-05 17:48:20 -0700

    got matches down from 200 ish to 5

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -4,51 +4,52 @@ fn pt1(input: &str) -> i32 {
     for (i, line) in chars.iter().enumerate() {
         for (j, char) in line.iter().enumerate() {
             if *char == 'X' {
-                println!("{},{}", i, j);
-                matches += check_path(4, &chars, (i as i32, j as i32));
+                matches += check_letter(&chars, (i as i32, j as i32));
             }
         }
     }
     return matches;
 }
 
-fn check_path(curr_letter: i32, chars: &Vec<Vec<char>>, idx: (i32, i32)) -> i32 {
+fn check_letter(chars: &Vec<Vec<char>>, idx: (i32, i32)) -> i32 {
     let curr_char = chars[idx.0 as usize][idx.1 as usize];
     let (h, w) = (chars.len() as i32, chars[0].len() as i32);
-    let paths: Vec<(i32, i32)> = vec![
-        (idx.0 - 1, idx.1 - 1),
-        (idx.0 - 1, idx.1),
-        (idx.0 - 1, idx.1 + 1),
-        (idx.0, idx.1 + 1),
-        (idx.0 + 1, idx.1 + 1),
-        (idx.0 + 1, idx.1),
-        (idx.0 + 1, idx.1 - 1),
-        (idx.0, idx.1 - 1),
+    let dirs = vec![
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (1, -1),
+        (0, -1),
     ]
     .into_iter()
-    .filter(|x| (x.0 >= 0 && x.1 >= 0 && x.0 < h && x.1 < w))
-    .collect();
-    if curr_letter == 4 {
-        if curr_char == 'X' {
-            return paths.iter().map(|x| check_path(3, chars, *x)).sum();
-        }
+    .filter(|x| (x.0 >= 0 && x.1 >= 0 && x.0 < h && x.1 < w));
+    if curr_char == 'X' {
+        return dirs
+            .map(|x| check_dir(3, chars, (idx.0 + x.0, idx.1 + x.1), x))
+            .sum();
+    }
+    return 0;
+    return 0;
+}
+
+fn check_dir(curr_letter: i32, chars: &Vec<Vec<char>>, idx: (i32, i32), dir: (i32, i32)) -> i32 {
+    if (idx.0 < 0 || idx.0 >= chars.len() as i32) {
         return 0;
-    } else if curr_letter == 3 {
-        if curr_char == 'M' {
-            return paths.iter().map(|x| check_path(2, chars, *x)).sum();
-        }
+    } else if (idx.1 < 0 || idx.1 >= chars[idx.0 as usize].len() as i32) {
         return 0;
-    } else if curr_letter == 2 {
-        if curr_char == 'A' {
-            return paths.iter().map(|x| check_path(1, chars, *x)).sum();
-        }
-        return 0;
-    } else if curr_letter == 1 {
-        if curr_char == 'S' {
-            println!("{}: {},{}", curr_char, idx.0, idx.1);
-            return 1;
-        }
-        return 0;
+    }
+    let curr_char = chars[idx.0 as usize][idx.1 as usize];
+    if curr_letter == 3 && curr_char == 'M' {
+        return check_dir(2, chars, (idx.0 + dir.0, idx.1 + dir.1), dir);
+    }
+    if curr_letter == 2 && curr_char == 'A' {
+        return check_dir(1, chars, (idx.0 + dir.0, idx.1 + dir.1), dir);
+    }
+    if curr_letter == 1 && curr_char == 'S' {
+        return 1;
     }
     return 0;
 }
